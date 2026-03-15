1e7e1c1b23e825ce84c7c93ac47944b918674b3f
Santosh-7124 <sunny962002@gmail.com>
2026-03-15 19:22:00 +0530

    Fix

--- src/components/About/Hero.jsx
+++ src/components/About/Hero.jsx
@@ -3,7 +3,7 @@ import Sample from "../../assets/About/Banner/About Video.mp4";
 
 function Hero() {
   return (
-    <section className="home-banner other-banner">
+    <section className="home-banner other-banner about-banner">
       <header>
         <h1>
           A Legacy of 25 Years in
